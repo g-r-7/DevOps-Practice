8a65a2b088c14c941301133693da7af237af46dc
g-r-7 <seckgora@ept.sn>
2024-11-29 15:52:13 +0000

    test scan et enregistrement résultats et déploiement

--- BackApp/util/database.js
+++ BackApp/util/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.PG_USER || 'postgres',
     process.env.PG_PASSWORD || 'password',
     {
-        host: process.env.PG_HOST || 'database',
+        host: process.env.PG_HOST || 'localhost',
         dialect: 'postgres',
     }
 );
